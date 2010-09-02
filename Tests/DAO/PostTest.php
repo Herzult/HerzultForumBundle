c2af941418c899dd7d11b476bde6f0efb606b367
ornicar <thibault.duplessis@gmail.com>
2010-09-02 15:17:26 -0700

    Update PostTest

--- Tests/DAO/PostTest.php
+++ Tests/DAO/PostTest.php
@@ -1,15 +1,24 @@
 <?php
 
 namespace Bundle\ForumBundle\Tests\DAO;
+use Bundle\ForumBundle\Test\WebTestCase;
 
-use Bundle\ForumBundle\Entity\Post;
-
-class PostTest extends \PHPUnit_Framework_TestCase
+class PostTest extends WebTestCase
 {
+    protected $postClass;
+
+    public function setUp()
+    {
+        parent::setUp();
+        if(null === $this->postClass) {
+            $this->postClass = $this->getService('forum.object_manager')->getRepository('ForumBundle:Post')->getObjectClass();
+        }
+    }
 
     public function testMessage()
     {
-        $post = new Post($this->getMock('Bundle\ForumBundle\Entity\Topic', array(), array(), '', false));
+        $class = $this->postClass;
+        $post = new $class($this->getMock('Bundle\ForumBundle\Entity\Topic', array(), array(), '', false));
         $this->assertEmpty($post->getMessage());
         $post->setMessage('Foo bar bla bla...');
         $this->assertEquals('Foo bar bla bla...', $post->getMessage());
@@ -17,7 +26,8 @@ class PostTest extends \PHPUnit_Framework_TestCase
 
     public function testCreatedAt()
     {
-        $post = new Post($this->getMock('Bundle\ForumBundle\Entity\Topic', array(), array(), '', false));
+        $class = $this->postClass;
+        $post = new $class($this->getMock('Bundle\ForumBundle\Entity\Topic', array(), array(), '', false));
         $this->assertEmpty($post->getCreatedAt());
         $post->setCreatedNow();
         $this->assertInstanceOf('\DateTime', $post->getCreatedAt());
@@ -26,7 +36,8 @@ class PostTest extends \PHPUnit_Framework_TestCase
 
     public function testUpdatedAt()
     {
-        $post = new Post($this->getMock('Bundle\ForumBundle\Entity\Topic', array(), array(), '', false));
+        $class = $this->postClass;
+        $post = new $class($this->getMock('Bundle\ForumBundle\Entity\Topic', array(), array(), '', false));
         $this->assertEmpty($post->getUpdatedAt());
         $post->setUpdatedNow();
         $this->assertInstanceOf('\DateTime', $post->getUpdatedAt());
