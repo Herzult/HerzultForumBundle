fa152f3a94a0a6f517f532c85de1dec7b788e3af
ornicar <thibault.duplessis@gmail.com>
2010-09-02 18:27:02 -0700

    Remove unnecessary namespaces in ForumBundle.php

--- ForumBundle.php
+++ ForumBundle.php
@@ -3,8 +3,6 @@
 namespace Bundle\ForumBundle;
 
 use Symfony\Framework\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class ForumBundle extends Bundle
 {
