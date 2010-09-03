1b71b640734b05bcb8d3e46b3d45a3a38284c37b
ornicar <thibault.duplessis@gmail.com>
2010-09-03 08:11:56 +0800

    Test Doctrine metadata factory getAllMetadata method. Actually it fails because ORM and ODM XML mapping files live in the same directory.

---
+++ Tests/DAO/MetadataTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bundle\ForumBundle\Tests\DAO;
+
+use Bundle\ForumBundle\Test\WebTestCase;
+
+class MetadataTest extends WebTestCase
+{
+
+    public function testGetAllMetadata()
+    {
+        $om = $this->getService('forum.object_manager');
+
+        try {
+            $allClasses = $om->getMetadataFactory()->getAllMetadata();
+        }
+        catch(\Exception $e) {
+            $this->fail(sprintf('%s %s', get_class($e), $e->getMessage()));
+        }
+
+        $this->pass();
+    }
+}
