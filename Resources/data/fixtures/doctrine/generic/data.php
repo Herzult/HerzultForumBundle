cfc77c705725d943d610a2d9dc8516107356af8c
ornicar <thibault.duplessis@gmail.com>
2010-09-28 16:21:14 +0200

    Update fixtures, make them work with both orm and mongodb

--- Resources/data/fixtures/doctrine/generic/data.php
+++ Resources/data/fixtures/doctrine/generic/data.php
@@ -1,70 +1,70 @@
 <?php
 
-use Bundle\DoctrineUserBundle\Entity\User;
-use Bundle\ForumBundle\Entity\Category;
-use Bundle\ForumBundle\Entity\Topic;
-use Bundle\ForumBundle\Entity\Post;
+$categoryClass = $this->container->getParameter('forum.category_object.class');
+$topicClass = $this->container->getParameter('forum.topic_object.class');
+$postClass = $this->container->getParameter('forum.post_object.class');
+$userClass = $this->container->get('doctrine_user.user_repository')->getObjectClass();
 
 /**
  * Create some users
  */
 $users = array();
 
-$john = new User();
+$john = new $userClass();
 $john->setUsername('John');
 $john->setEmail('john@doe.com');
 $john->setPassword('changeme');
 $users[] = $john;
 
-$tom = new User();
+$tom = new $userClass();
 $tom->setUsername('Tom');
 $tom->setEmail('tom@doe.com');
 $tom->setPassword('changeme');
 $users[] = $tom;
 
-$max = new User();
+$max = new $userClass();
 $max->setUsername('Max');
 $max->setEmail('max@doe.com');
 $max->setPassword('changeme');
 $users[] = $max;
 
-$marie = new User();
+$marie = new $userClass();
 $marie->setUsername('Marie');
 $marie->setEmail('marie@doe.com');
 $marie->setPassword('changeme');
 $users[] = $marie;
 
-$thomas = new User();
+$thomas = new $userClass();
 $thomas->setUsername('Thomas');
 $thomas->setEmail('thomas@doe.com');
 $thomas->setPassword('changeme');
 $users[] = $thomas;
 
-$julie = new User();
+$julie = new $userClass();
 $julie->setUsername('Julie');
 $julie->setEmail('julie@doe.com');
 $julie->setPassword('changeme');
 $users[] = $julie;
 
-$marc = new User();
+$marc = new $userClass();
 $marc->setUsername('Marc');
 $marc->setEmail('marc@doe.com');
 $marc->setPassword('changeme');
 $users[] = $marc;
 
-$paul = new User();
+$paul = new $userClass();
 $paul->setUsername('Paul');
 $paul->setEmail('paul@doe.com');
 $paul->setPassword('changeme');
 $users[] = $paul;
 
-$martin = new User();
+$martin = new $userClass();
 $martin->setUsername('Martin');
 $martin->setEmail('martin@doe.com');
 $martin->setPassword('changeme');
 $users[] = $martin;
 
-$kate = new User();
+$kate = new $userClass();
 $kate->setUsername('Kate');
 $kate->setEmail('kate@doe.com');
 $kate->setPassword('changeme');
@@ -77,22 +77,22 @@ $users[] = $kate;
 
 $categories = array();
 
-$general = new Category();
+$general = new $categoryClass();
 $general->setName('General');
 $general->setPosition(0);
 $categories[] = $general;
 
-$documentation = new Category();
+$documentation = new $categoryClass();
 $documentation->setName('Documentation');
 $documentation->setPosition(1);
 $categories[] = $documentation;
 
-$helpAndSupport = new Category();
+$helpAndSupport = new $categoryClass();
 $helpAndSupport->setName('Help & Support');
 $helpAndSupport->setPosition(2);
 $categories[] = $helpAndSupport;
 
-$development = new Category();
+$development = new $categoryClass();
 $development->setName('Development');
 $development->setPosition(3);
 $categories[] = $development;
@@ -103,31 +103,29 @@ $categories[] = $development;
  * Create some topics
  */
 
-for ($i = 0; $i < 100; $i++) {
-    $topic = new Topic();
-    $topic->setCategory($categories[mt_rand(0, count($categories) - 1)]);
+for ($i = 0; $i < 50; $i++) {
+    $topic = new $topicClass();
+    $topic->setCategory($categories[$i%(count($categories)-1) + 1]);
     $topic->setSubject('Test topic number ' . $i);
-    $topic->setAuthor($user = $users[mt_rand(0, count($users) - 1)]);
+    $topic->setAuthor($users[$i%(count($users)-1) + 1]);
 
-    $ii = 0;
+    for($itPost = 0; $itPost < ($i%10)+1; $itPost++) {
 
-    do {
-
-        if ($ii > 0) {
-            $user = $users[mt_rand(0, count($users) - 1)];
+        if(0 === $itPost) {
+            $user = $topic->getAuthor();
+        }
+        else {
+            $user = $users[$itPost%(count($users)-1) + 1];
         }
 
-        $post = new Post($topic);
+        $post = new $postClass($topic);
         $post->setMessage('Mauris vestibulum, neque sed dictum eleifend, nunc risus varius orci, in consequat enim diam vel arcu. Curabitur ut odio vel est tempor bibendum. Donec felis orci, adipiscing non, luctus sit amet, faucibus ut, nulla. Cras eu tellus eu augue porttitor interdum. Sed auctor odio a purus. Duis elementum, dui quis accumsan convallis, ante lectus convallis est.');
         $post->setAuthor($user);
 
-        ${'post_' . $i . '_' . $ii} = $post;
-
-        unset($post);
-        $ii++;
-    } while (mt_rand(0, 1) === 1);
+        ${'post_' . $i . '_' . $itPost} = $post;
+    }
 
     ${'topic_' . $i} = $topic;
 
-    unset($topic, $user);
+    unset($post, $topic, $user);
 }
---
+++ Resources/data/fixtures/doctrine/orm/data.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once __DIR__.'/../generic/data.php';
