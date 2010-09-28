5df14266398d2b1cf5ea29a88b8b8ddacd9e48c3
ornicar <thibault.duplessis@gmail.com>
2010-09-28 18:07:01 +0200

    Move category show action to the category controller

--- Controller/CategoryController.php
+++ Controller/CategoryController.php
@@ -10,6 +10,24 @@ class CategoryController extends Controller
     {
         $categories = $this['forum.category_repository']->findAll();
 
-        return $this->render('ForumBundle:Category:list', array('categories' => $categories));
+        return $this->render('ForumBundle:Category:list:'.$this->getRenderer(), array('categories' => $categories));
+    }
+
+    public function showAction($slug)
+    {
+        $category = $this['forum.category_repository']->findOneBySlug($slug);
+
+        if (!$category) {
+            throw new NotFoundHttpException('The topic does not exist.');
+        }
+
+        return $this->render('ForumBundle:Category:show:'.$this->getRenderer(), array(
+            'category' => $category
+        ));
+    }
+
+    protected function getRenderer()
+    {
+        return $this->container->getParameter('forum.template.renderer');
     }
 }
--- Controller/ForumController.php
+++ Controller/ForumController.php
@@ -13,20 +13,6 @@ class ForumController extends Controller
         return $this->render('ForumBundle:Forum:index');
     }
 
-    public function categoryAction($category_slug)
-    {
-        $category = $this['forum.category_repository']->findOneBySlug($category_slug);
-
-        if (!$category) {
-            throw new NotFoundHttpException('The topic does not exist.');
-        }
-
-        return $this->render('ForumBundle:Forum:category:'.$this->getRenderer(), array(
-            'category' => $category
-        ));
-    }
-
-
     protected function getRenderer()
     {
         return $this->container->getParameter('forum.template.renderer');
