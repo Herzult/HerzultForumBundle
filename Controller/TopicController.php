1a08c78825929ca03ee9df9eac5012a7aff5189e
ornicar <thibault.duplessis@gmail.com>
2010-10-01 17:16:46 +0200

    Add Topic creation controller and routing

--- Controller/TopicController.php
+++ Controller/TopicController.php
@@ -2,11 +2,37 @@
 
 namespace Bundle\ForumBundle\Controller;
 
+use Bundle\ForumBundle\Form\TopicForm;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class TopicController extends Controller
 {
+    public function newAction()
+    {
+        $form = $this->createForm('forum_topic_new');
+
+        return $this->render('ForumBundle:Topic:new.'.$this->getRenderer(), array('form' => $form));
+    }
+
+    public function createAction()
+    {
+        $form = $this->createForm('forum_topic_new');
+
+        $form->bind($this['request']->request->get($form->getName()));
+
+        if ($form->isValid()) {
+            $topic = $form->getData();
+            $this->saveTopic($topic);
+
+            $this['session']->setFlash('forum_topic_create/success', true);
+            $url = $this->generateUrl('forum_topic_show', array('id' => $topic->getId()));
+
+            return $this->redirect($url);
+        }
+
+        return $this->render('ForumBundle:Topic:new.'.$this->getRenderer(), array('form' => $form));
+    }
 
     public function listAction($category = null)
     {
@@ -33,5 +59,36 @@ class TopicController extends Controller
     protected function getRenderer()
     {
         return $this->container->getParameter('forum.template.renderer');
+    }
+
+    /**
+     * Create a TopicForm instance and returns it 
+     * 
+     * @param string $name 
+     * @param Topic $topic 
+     * @return Bundle\ForumBundle\Form\TopicForm
+     */
+    protected function createForm($name, $topic = null)
+    {
+        $formClass = $this->container->getParameter('forum.topic_form.class');
+        if (null === $object) {
+            $topicClass = $this['forum.topic_repository']->getObjectClass();
+            $topic = new $topicClass();
+        }
+
+        return new $formClass($name, $topic, $this['validator']);
+    }
+
+    /**
+     * Save a topic in database
+     *
+     * @param Topic $topic
+     * @return null
+     **/
+    public function saveTopic(Topic $topic)
+    {
+        $objectManager = $this['forum.topic_repository']->getObjectManager();
+        $objectManager->persist($topic);
+        $objectManager->flush();
     }
 }
