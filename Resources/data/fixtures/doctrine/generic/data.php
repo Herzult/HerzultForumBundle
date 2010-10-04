6b9045f33a0bca8d58a5e3af575752969c0f5a49
ornicar <thibault.duplessis@gmail.com>
2010-10-04 20:01:41 +0200

    Fix fixtures

--- Resources/data/fixtures/doctrine/generic/data.php
+++ Resources/data/fixtures/doctrine/generic/data.php
@@ -122,7 +122,8 @@ for ($i = 0; $i < 50; $i++) {
             $user = $users[$itPost%(count($users)-1) + 1];
         }
 
-        $post = new $postClass($topic);
+        $post = new $postClass();
+        $post->setTopic($topic);
         $post->setMessage('Mauris vestibulum, neque sed dictum eleifend, nunc risus varius orci, in consequat enim diam vel arcu. Curabitur ut odio vel est tempor bibendum. Donec felis orci, adipiscing non, luctus sit amet, faucibus ut, nulla. Cras eu tellus eu augue porttitor interdum. Sed auctor odio a purus. Duis elementum, dui quis accumsan convallis, ante lectus convallis est.');
         $post->setAuthor($user);
 
