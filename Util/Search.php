d3ca58e694aac7ca68c98e320d11ad7ecfaf2148
ornicar <thibault.duplessis@gmail.com>
2010-10-05 16:37:02 +0200

    Add validation annotations to the search query

--- Util/Search.php
+++ Util/Search.php
@@ -4,5 +4,11 @@ namespace Bundle\ForumBundle\Util;
 
 class Search
 {
+    /**
+     * @Validation({
+     *   @NotBlank(),
+     *   @MinLength(limit=3, message="Just a little too short.")
+     * })
+     */
     public $query;
 }
