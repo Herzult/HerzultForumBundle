a521ed1c1ca39fdc61a04370090fc7944c7806f5
ornicar <thibault.duplessis@gmail.com>
2010-10-05 16:14:29 +0200

    Add search controller and routing

--- Controller/ForumController.php
+++ Controller/ForumController.php
@@ -3,12 +3,20 @@
 namespace Bundle\ForumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Bundle\ForumBundle\Form\SearchForm;
 
 class ForumController extends Controller
 {
     public function indexAction()
     {
         return $this->render('ForumBundle:Forum:index.'.$this->getRenderer());
+    }
+
+    public function searchAction()
+    {
+        $form = new SearchForm('search', array(), $this['validator']);
+        return $this->render('ForumBundle:Forum:search.php', array('form' => $form));
+        return $this->render('ForumBundle:Forum:search.'.$this->getRenderer(), array('form' => $form));
     }
 
     protected function getRenderer()
