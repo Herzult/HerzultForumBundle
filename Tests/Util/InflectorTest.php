f7fe43e65b2ddd5b789456deaf274849adfc4fb6
ornicar <thibault.duplessis@gmail.com>
2010-10-14 11:25:27 +0200

    Remove tests that require iconv

--- Tests/Util/InflectorTest.php
+++ Tests/Util/InflectorTest.php
@@ -17,13 +17,6 @@ class InflectorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('foo-bar', Inflector::slugify('Foo-Bar'));
         $this->assertEquals('foo-bar', Inflector::slugify('Foo / Bar'));
         $this->assertEquals('foo-bar', Inflector::slugify('Foo Bar ...'));
-
-        if (function_exists('iconv')) {
-            $this->assertEquals('mr-francois-dupont', Inflector::slugify('Mr. François Dupont'));
-            $this->assertEquals('le-numero-de-telephone', Inflector::slugify('Le numéro de téléphone'));
-        } else {
-            $this->markTestSkipped('Removing accents: iconv not installed');
-        }
     }
 
 }
