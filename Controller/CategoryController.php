8dd56fc1c529fb3f96e1b4bb1ad1ec3e96e982f7
ornicar <thibault.duplessis@gmail.com>
2010-10-17 02:14:00 +0200

    Add missing namespace in category controller

--- Controller/CategoryController.php
+++ Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace Bundle\ForumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategoryController extends Controller
 {
