11d19d5d040eb50d503ccbfac470f12f55467f2e
Antoine Hérault <antoine.herault@gmail.com>
2010-10-23 01:02:55 +0200

    introduce twig templates

--- Resources/views/Post/form.php
+++
@@ -1,7 +0,0 @@
-<?php echo $form->renderErrors() ?>
-<?php echo $form->renderHiddenFields() ?>
-<div class="field message">
-    <?php echo $form['message']->renderErrors() ?>
-    <label for="<?php echo $form['message']->getId() ?>">Message</label>
-    <?php echo $form['message']->render() ?>
-</div>
--- Resources/views/Topic/list.xml.php
+++
@@ -1,9 +0,0 @@
-<?php foreach ($topics as $topic): ?>
-    <entry>
-        <title><?php echo $topic->getSubject() ?></title>
-        <author><?php echo $topic->getAuthor() ? : 'Anonymous' ?></author>
-        <updated><?php echo $topic->getPulledAt()->format('c') ?></updated>
-        <id><?php echo $view['forum']->urlForTopic($topic, true) ?></id>
-        <link rel="alternate"><?php echo $view['forum']->urlForTopic($topic, true) ?></link>
-    </entry>
-<?php endforeach ?>
