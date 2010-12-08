79ef2a670b213afac9e463c76e64e9b4f86bd8e2
ornicar <thibault.duplessis@gmail.com>
2010-12-08 17:25:51 +0100

    Add TopicInterface::findOneByCategoryAndSlug and implement MongoDB repository

--- Document/TopicRepository.php
+++ Document/TopicRepository.php
@@ -8,6 +8,16 @@ use ZendPaginatorAdapter\DoctrineMongoDBAdapter;
 
 class TopicRepository extends ObjectRepository implements TopicRepositoryInterface
 {
+    /**
+     * @see TopicRepositoryInterface::findOneByCategoryAndSlug
+     */
+    public function findOneByCategoryAndSlug(Category $category, $slug)
+    {
+        return $this->findOneBy(array(
+            'slug' => $slug,
+            'category.$id' => new \MongoId($category->getId())
+        ));
+    }
 
     /**
      * @see TopicRepositoryInterface::findOneById
