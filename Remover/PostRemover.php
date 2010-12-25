ed1f73da9b61f84b2bf34837d187cd861257395d
ornicar <thibault.duplessis@gmail.com>
2010-12-25 19:30:59 +0100

    Prevent removal of the first post of a topic

--- Remover/PostRemover.php
+++ Remover/PostRemover.php
@@ -18,6 +18,10 @@ class PostRemover
 
     public function remove(Post $post)
     {
+        if(1 === $post->getTopic()->getNumPosts()) {
+            throw new LogicException('You shall not remove the first topic post. Remove the topic instead');
+        }
+
         $this->objectManager->remove($post);
 
         // Must flush because the topic updater will fetch posts from DB
