50feb2929b2a426fbeafc6ba22748b3703ad5b0c
ornicar <thibault.duplessis@gmail.com>
2010-12-25 15:32:15 +0100

    Add category updater with unit test

---
+++ Tests/Updater/CategoryUpdaterTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Bundle\ForumBundle\Updater;
+
+class CategoryUpdaterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testUpdate()
+    {
+        $lastPost1 = $this->createPost();
+        $lastPost1->expects($this->once())
+            ->method('isPosteriorTo')
+            ->will($this->returnValue(true));
+        $topic1 = $this->createTopic($lastPost1);
+
+        $lastPost2 = $this->createPost();
+        $lastPost2->expects($this->once())
+            ->method('isPosteriorTo')
+            ->with($lastPost1)
+            ->will($this->returnValue(true));
+        $topic2 = $this->createTopic($lastPost2);
+
+        $topics = array($topic1, $topic2);
+
+        $category = $this->getMockBuilder('Bundle\ForumBundle\Model\Category')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $category->expects($this->once())
+            ->method('setNumTopics')
+            ->with(2);
+        $category->expects($this->once())
+            ->method('setNumPosts')
+            ->with(8);
+        $category->expects($this->once())
+            ->method('setLastTopic')
+            ->with($topic2);
+        $category->expects($this->once())
+            ->method('setLastPost')
+            ->with($lastPost2);
+
+        $topicRepository = $this->getMockBuilder('Bundle\ForumBundle\Model\TopicRepositoryInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $topicRepository->expects($this->once())
+            ->method('findAllByCategory')
+            ->with($category)
+            ->will($this->returnValue($topics));
+
+        $updater = new CategoryUpdater($topicRepository);
+        $updater->update($category);
+    }
+
+    public function createPost()
+    {
+        return $this->getMockBuilder('Bundle\ForumBundle\Model\Post')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function createTopic($lastPost)
+    {
+        $topic = $this->getMockBuilder('Bundle\ForumBundle\Model\Topic')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $topic->expects($this->once())
+            ->method('getNumPosts')
+            ->will($this->returnValue(4));
+        $topic->expects($this->once())
+            ->method('getLastPost')
+            ->will($this->returnValue($lastPost));
+
+        return $topic;
+    }
+}
---
+++ Updater/CategoryUpdater.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bundle\ForumBundle\Updater;
+
+use Bundle\ForumBundle\Model\Category;
+use Bundle\ForumBundle\Model\TopicRepositoryInterface;
+
+class CategoryUpdater
+{
+    protected $topicRepository;
+
+    public function __construct(TopicRepositoryInterface $topicRepository)
+    {
+        $this->topicRepository = $topicRepository;
+    }
+
+    public function update(Category $category)
+    {
+        $topics = $this->topicRepository->findAllByCategory($category, false);
+        $category->setNumTopics(count($topics));
+        $numPosts = 0;
+        $lastPost = $lastTopic = null;
+        foreach($topics as $topic) {
+            $numPosts += $topic->getNumPosts();
+            $topicLastPost = $topic->getLastPost();
+            if($topicLastPost->isPosteriorTo($lastPost)) {
+                $lastPost = $topicLastPost;
+                $lastTopic = $topic;
+            }
+        }
+        $category->setNumPosts($numPosts);
+        $category->setLastPost($lastPost);
+        $category->setLastTopic($lastTopic);
+    }
+}
