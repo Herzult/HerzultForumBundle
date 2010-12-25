0969be9d31cf5f3d5241794f6267b7121702bc96
ornicar <thibault.duplessis@gmail.com>
2010-12-25 18:54:19 +0100

    Add post creator with unit test

---
+++ Creator/PostCreator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bundle\ForumBundle\Creator;
+
+use Bundle\ForumBundle\Model\Post;
+use DateTime;
+use LogicException;
+
+class PostCreator
+{
+    public function create(Post $post)
+    {
+        if(!$topic = $post->getTopic()) {
+            throw new LogicException('Each post must have a topic');
+        }
+        if(!$category = $topic->getCategory()) {
+            throw new LogicException('Each topic must have a category');
+        }
+
+        if(!$topic->getFirstPost()) {
+            $topic->setFirstPost($post);
+        }
+        $topic->incrementNumPosts();
+        $topic->setLastPost($post);
+        $topic->setPulledAt(new DateTime());
+
+        $category->setLastPost($post);
+        $category->incrementNumPosts();
+
+        $post->setNumber($topic->getNumPosts());
+    }
+}
---
+++ Tests/Creator/PostCreatorTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Bundle\ForumBundle\Creator;
+
+class TopicUpdaterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testUpdate()
+    {
+        $post = $this->getMockBuilder('Bundle\ForumBundle\Model\Post')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $category = $this->getMockBuilder('Bundle\ForumBundle\Model\Category')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $category->expects($this->once())
+            ->method('setLastPost')
+            ->with($post);
+        $category->expects($this->once())
+            ->method('incrementNumPosts');
+
+        $topic = $this->getMockBuilder('Bundle\ForumBundle\Model\Topic')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $topic->expects($this->once())
+            ->method('getCategory')
+            ->will($this->returnValue($category));
+        $topic->expects($this->once())
+            ->method('getFirstPost')
+            ->will($this->returnValue(null));
+        $topic->expects($this->once())
+            ->method('setFirstPost')
+            ->with($post);
+        $topic->expects($this->once())
+            ->method('incrementNumPosts');
+        $topic->expects($this->once())
+            ->method('setLastPost')
+            ->with($post);
+        $topic->expects($this->once())
+            ->method('setPulledAt');
+        $topic->expects($this->once())
+            ->method('getNumPosts')
+            ->will($this->returnValue(10));
+
+        $post->expects($this->once())
+            ->method('getTopic')
+            ->will($this->returnValue($topic));
+        $post->expects($this->once())
+            ->method('setNumber')
+            ->with(10);
+
+        $creator = new PostCreator();
+        $creator->create($post);
+    }
+}
