d5dbdeeeedf07351c066b99dd386edaa98a3696d
ornicar <thibault.duplessis@gmail.com>
2011-01-10 04:02:24 +0100

    Update twig extension

--- Twig/Extension/ForumExtension.php
+++ Twig/Extension/ForumExtension.php
@@ -1,21 +1,32 @@
 <?php
 
 namespace Bundle\ForumBundle\Twig\Extension;
-use Symfony\Bundle\TwigBundle\TokenParser\HelperTokenParser;
+use Bundle\ForumBundle\Templating\Helper\ForumHelper;
 
 class ForumExtension extends \Twig_Extension
 {
+    protected $helper;
+
+    public function __construct(ForumHelper $helper)
+    {
+        $this->helper = $helper;
+    }
+
     /**
-     * Returns the token parser instance to add to the existing list.
+     * Returns a list of global functions to add to the existing list.
      *
-     * @return array An array of Twig_TokenParser instances
+     * @return array An array of global functions
      */
-    public function getTokenParsers()
+    public function getFunctions()
     {
         return array(
-            // {% forum_post_route post %}
-            new HelperTokenParser('forum_post_route', '<post>', 'forum.templating.helper.forum', 'urlForPost')
+            'forum_post_route' => new \Twig_Function_Method($this, 'postRoute')
         );
+    }
+
+    public function postRoute($post)
+    {
+        return $this->helper->urlForPost($post);
     }
 
     public function getName()
