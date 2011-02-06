7582e011b425fd96370e60434ca54ba0a618155a
ornicar <thibault.duplessis@gmail.com>
2011-02-06 15:53:05 -0800

    Improve repository service construction

--- ForumBundle.php
+++ ForumBundle.php
@@ -8,18 +8,6 @@ use DoctrineExtensions\Sluggable\SluggableListener;
 class ForumBundle extends Bundle
 {
 
-    /**
-     * Get a EntityRepository or a DocumentRepository, based on db driver configuration
-     *
-     * @param mixed $objectManager a EntityManager or a DocumentManager
-     * @param mixed $objectClass the class of the entity or document
-     * @return mixed a EntityRepository or DocumentRepository
-     */
-    public static function getRepository($objectManager, $objectClass)
-    {
-        return $objectManager->getRepository($objectClass);
-    }
-
     public function boot()
     {
         $eventManager = $this->container->get('forum.object_manager')->getEventManager();
@@ -40,6 +28,6 @@ class ForumBundle extends Bundle
      */
     public function getPath()
     {
-        return strtr(__DIR__, '\\', '/');
+        return __DIR__;
     }
 }
