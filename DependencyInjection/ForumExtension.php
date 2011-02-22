e8d3ae3353156f93dbc03e4f2b790678e5391c24
ornicar <thibault.duplessis@gmail.com>
2011-02-21 22:58:05 -0800

    Simplify DIC extension

--- DependencyInjection/ForumExtension.php
+++ DependencyInjection/ForumExtension.php
@@ -2,7 +2,7 @@
 
 namespace Bundle\ForumBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Loader\FileLoader;
@@ -97,37 +97,4 @@ class ForumExtension extends Extension
             }
         }
     }
-
-    /**
-     * Returns the namespace to be used for this extension (XML namespace).
-     *
-     * @return string The XML namespace
-     */
-    public function getNamespace()
-    {
-        return 'http://www.symfony-project.org/shemas/dic/symfony';
-    }
-
-    /**
-     * Returns the base path for the XSD files.
-     *
-     * @return string The XSD base path
-     */
-    public function getXsdValidationBasePath()
-    {
-        return null;
-    }
-
-    /**
-     * Returns the recommended alias to use in XML.
-     *
-     * This alias is also the mandatory prefix to use when using YAML.
-     *
-     * @return string The alias
-     */
-    public function getAlias()
-    {
-        return 'forum';
-    }
-
 }
