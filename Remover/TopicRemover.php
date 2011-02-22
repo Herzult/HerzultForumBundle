c576a13f63f7bf978bc9fc3db75ab20f6148b804
ornicar <thibault.duplessis@gmail.com>
2011-02-22 00:12:37 -0800

    Add topic remover service

---
+++ Remover/TopicRemover.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Bundle\ForumBundle\Remover;
+
+use Bundle\ForumBundle\Model\Topic;
+use Bundle\ForumBundle\Model\PostRepositoryInterface;
+use Bundle\ForumBundle\Updater\CategoryUpdater;
+use LogicException;
+
+class TopicRemover
+{
+    protected $objectManager;
+    protected $postRepository;
+    protected $categoryUpdater;
+
+    public function __construct($objectManager, PostRepositoryInterface $postRepository, CategoryUpdater $categoryUpdater)
+    {
+        $this->objectManager   = $objectManager;
+        $this->postRepository  = $postRepository;
+        $this->categoryUpdater = $categoryUpdater;
+    }
+
+    public function remove(Topic $topic)
+    {
+        $category = $topic->getCategory();
+
+        if(1 === $category->getNumTopics()) {
+            throw new LogicException('You shall not remove the first category topic. Remove the category instead');
+        }
+
+        foreach ($this->getTopicPosts($topic) as $post) {
+            $this->objectManager->remove($post);
+        }
+        $this->objectManager->remove($topic);
+
+        // Must flush because the category updater will fetch topics from DB
+        $this->objectManager->flush();
+
+        $this->categoryUpdater->update($category);
+    }
+
+    protected function getTopicPosts(Topic $topic)
+    {
+        return $this->postRepository->findAllByTopic($topic);
+    }
+}
