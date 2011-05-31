8e2eb1ad08ad0d373836400908bb82ef880aea7c
Antoine Berranger <lysimaque@gmail.com>
2011-05-31 07:40:17 -0700

    updating validation annotations

--- Search/Search.php
+++ Search/Search.php
@@ -2,12 +2,14 @@
 
 namespace Bundle\ForumBundle\Search;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Search
 {
     /**
-     * @assert:Validation({
-     *   @assert:NotBlank(),
-     *   @assert:MinLength(limit=3, message="Just a little too short.")
+     * @Assert\Validation({
+     *   @Assert\NotBlank(),
+     *   @Assert\MinLength(limit=3, message="Just a little too short.")
      * })
      */
     public $query;
