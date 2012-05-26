3dcf22794521a904ec42ee960399f8284bed15e2
Joris de Wit <joris.w.dewit@gmail.com>
2012-05-26 09:11:22 -0700

    updated Form to 2.1

--- Form/PostFormType.php
+++ Form/PostFormType.php
@@ -3,11 +3,11 @@
 namespace Herzult\Bundle\ForumBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class PostFormType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('message', 'textarea');
     }
